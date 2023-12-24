73c7fabeac0ba5e01fa88332a738ddc313b56b5b
suraj <surajjosh7@gmail.com>
2023-12-24 12:28:24 +0530

    new

---
+++ 08_user_interaction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#User interaction is taking input from user through shell or terminal
+
+# take input and store in name variable than print it
+read name
+echo "you name is" $name
+
+
+# take input with some text shown on shell and store input in name variable
+read -p "what is your name::" name 
+echo "hi " $name
