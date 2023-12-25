5270dcf0fccae934d86fc132f437706ffdc41d23
suraj <surajjosh7@gmail.com>
2023-12-25 15:45:10 +0530

    new

--- 09_airthmatic_operation.sh
+++ 09_airthmatic_operation.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-# Mathematic calculations
+# Addition
+echo "Addition:"
+a=10
+b=5
+sum=$((a + b))
+echo "Sum: $sum"
+
+# Subtraction
+echo "Subtraction:"
+difference=$((a - b))
+echo "Difference: $difference"
+
+# Multiplication
+echo "Multiplication:"
+product=$((a * b))
+echo "Product: $product"
+
+# Division
+echo "Division:"
+div=$((a / b))
+echo "Division: $div"
+
+# Modulus (Remainder)
+echo "Modulus:"
+mod=$((a % b))
+echo "Modulus: $mod"
+
+# Exponentiation (Using 'bc' for floating-point arithmetic)
+echo "Exponentiation:"
+echo "2^3 = $(echo '2^3' | bc -l)"
